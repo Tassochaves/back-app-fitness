67cf3d7c1aa93475050d2257ce1957a398157477
Tassochaves <tassogt@hotmail.com>
2024-09-15 22:27:41 -0300

    Add: controller de Atividade com POST.

---
+++ src/main/java/com/dev/app_fitness/controller/AtividadeController.java
@@ -0,0 +1,35 @@
+package com.dev.app_fitness.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.app_fitness.dto.AtividadeDTO;
+import com.dev.app_fitness.service.atividade.AtividadeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class AtividadeController {
+
+    private final AtividadeService atividadeService;
+
+    @PostMapping("/atividade")
+    public ResponseEntity<?> postarAtividade(@RequestBody AtividadeDTO dto){
+
+        AtividadeDTO criarAtividade = atividadeService.postarAtividade(dto);
+
+        if(criarAtividade != null){
+            return ResponseEntity.status(HttpStatus.CREATED).body(criarAtividade);
+        }else{
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao criar atividade!"); 
+        }
+    }
+}
